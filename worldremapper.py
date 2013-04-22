7780ed9809d0fa7df977ec50c4a65595b9282c93
Agaricus <agaricusb@yahoo.com>
2013-04-21 18:40:19 -0700

    Decompress chunk data

--- worldremapper.py
+++ worldremapper.py
@@ -3,6 +3,9 @@
 import sys
 import os
 import numpy
+import struct
+import gzip
+import zlib
 
 # based on pymclevel
 SECTOR_BYTES = 4096
@@ -10,6 +13,9 @@ SECTOR_INTS = SECTOR_BYTES / 4
 CHUNK_HEADER_SIZE = 5
 VERSION_GZIP = 1
 VERSION_DEFLATE = 2
+VERSION_GZIP = 1
+VERSION_DEFLATE = 2
+
 
 def main():
     if len(sys.argv) < 2:
@@ -20,13 +26,14 @@ def main():
 
     load(sys.argv[1])
 
+
 def load(path):
-    data = file(path, "rb").read()
-    filesize = len(data)
+    regionData = file(path, "rb").read()
+    filesize = len(regionData)
 
     n = 0
-    offsetsData = data[0:SECTOR_BYTES]
-    modTimesData = data[SECTOR_BYTES:SECTOR_BYTES * 2]
+    offsetsData = regionData[0:SECTOR_BYTES]
+    modTimesData = regionData[SECTOR_BYTES:SECTOR_BYTES * 2]
 
     freeSectors = [True] * (filesize / SECTOR_BYTES)
     freeSectors[0:2] = False, False
@@ -46,12 +53,40 @@ def load(path):
 
             freeSectors[i] = False
 
-    def getOffset(self, cx, cz):
+    def getOffset(cx, cz):
         cx &= 0x1f
         cz &= 0x1f
         return offsets[cx + cz * 32]
 
-    print 
+    def readChunk(cx, cz):
+        cx &= 0x1f
+        cz &= 0x1f
+        offset = getOffset(cx, cz)
+        assert offset != 0, "chunk not present: (%s, %s)" % (cx, cz)
+
+        sectorStart = offset >> 8
+        numSectors = offset & 0xff
+        assert numSectors != 0, "chunk not present 2: (%s, %s)" % (cx, cz)
+
+        assert sectorStart + numSectors <= len(freeSectors), "chunk not present 3: (%s, %s)" % (cx, cz)
+
+        start = sectorStart * SECTOR_BYTES
+        chunkData = regionData[start:start + numSectors * SECTOR_BYTES]
+        assert len(chunkData) >= 5, "Chunk data is only %d bytes long (expected 5) at (%s, %s)" % (len(chunkData), cx, cz)
+
+        length = struct.unpack_from(">I", chunkData)[0]
+        format = struct.unpack_from("B", chunkData, 4)[0]
+        chunkData = chunkData[5:length + 5]
+
+        # decompress
+        if format == VERSION_GZIP:
+            return gzip.GzipFile(fileobj=StringIO(chunkData)).read()
+        if format == VERSION_DEFLATE:
+            return zlib.decompress(chunkData)
+
+        assert False, "unsupported chunk compression format: %s at (%s, %s)" % (format, cx, cz)
+
+    print [readChunk(0,0)]
 
 if __name__ == "__main__":
     main()
