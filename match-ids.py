c0c0f5c6f2eb93591d7b73ac8b54c52a243da07c
Agaricus <agaricusb@yahoo.com>
2013-04-21 21:25:58 -0700

    Add matching IDs based on configuration file keys
    .. but only two hits, both false positives ('next'/'prev' in NEI) :(

--- match-ids.py
+++ match-ids.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import sys
+import os
+import re
 
 """Load an NEI ID map dump into a dictionary of unlocalized name -> numeric ID"""
 def readNEIDump(fn):
@@ -16,10 +18,11 @@ def readNEIDump(fn):
 
     return m
 
-def matchAll(before, after):
+def matchAll(before, after, configsBefore, configsAfter):
     mapping = {}
 
     for oldName, oldID in before.iteritems():
+        # check NEI dumps first
         if after.has_key(oldName):
             # exact match
             newName = oldName
@@ -36,19 +39,69 @@ def matchAll(before, after):
             del after[newName]
             continue
 
+        # search configs for likely configuration names, by ID
+        possibleOldNames = []
+        for config, keys in configsBefore.iteritems():
+            for key, id in keys.iteritems():
+                if id == oldID:
+                    possibleOldNames.append(key)
+
+                if id > 4095 and id - 256 == oldID:
+                    possibleOldNames.append(key)
+
+        # .. then try to match with new configs by name
+        possibleNewNames = []
+        for possibleOldName in possibleOldNames:
+            for config, keys in configsAfter.iteritems():
+                for key, id in keys.iteritems():
+                    if key == possibleOldName:
+                        possibleNewNames.append((config, key, id))
+        
+
+        print oldID,possibleOldNames,possibleNewNames
+
         mapping[oldID] = (None, oldName, None, "no match")
 
     return mapping
 
+def scanConfigs(dirname):
+    configKeys = {}
+    for root, dirs, files in os.walk(dirname):
+        for f in files:
+            path = os.path.join(root, f)
+            configKeys[path] = scanConfig(path)
+
+    return configKeys
+
+"""Get key/value pairs for approximately-read configuration file."""
+def scanConfig(fn):
+    keys = {}
+
+    for line in file(fn, "rt").readlines():
+        line = line.replace("\n", "")
+        line = line.strip()
+
+        match = re.match(r"(\w+)=(\d+)", line)
+        if match:
+            key = match.group(1)
+            id = int(match.group(2))
+
+            keys[key] = id
+
+    return keys
+
 def main():
-    if len(sys.argv) < 3:
-        print "usage: %s first-nei-dump second-nei-dump" % (sys.argv[0],)
+    if len(sys.argv) < 5:
+        print "usage: %s first-nei-dump second-nei-dump first-configs-dir second-configs-dir" % (sys.argv[0],)
         sys.exit(1)
 
     before = readNEIDump(sys.argv[1])
     after = readNEIDump(sys.argv[2])
 
-    mapping = matchAll(before, after)
+    configsBefore = scanConfigs(sys.argv[3])
+    configsAfter = scanConfigs(sys.argv[4])
+
+    mapping = matchAll(before, after, configsBefore, configsAfter)
 
     for k, v in mapping.iteritems():
         if v[0] is not None:
