4245b66e90f8b41e8faaed970e9b2d630e125fef
Agaricus <agaricusb@yahoo.com>
2013-04-21 21:04:12 -0700

    Add name matching to generate before->after ID mappings

--- compare-nei.py
+++ compare-nei.py
@@ -15,14 +15,67 @@ def readNEIDump(fn):
             m[unlocalizedName] = int(id)
 
     return m
- 
+
+def matchAll(before, after):
+    mapping = {}
+
+    for oldName, oldID in before.iteritems():
+        if after.has_key(oldName):
+            # exact match
+            newName = oldName
+            newID = after[newName]
+            mapping[oldID] = (newID, oldName, newName, "exact")
+            del after[newName]
+            continue
+
+        newName = oldName.replace("item.", "").replace("tile.", "")
+        if after.has_key(newName):
+            # IC2 dropped the loc prefixes..don't know why
+            newID = after[newName]
+            mapping[oldID] = (newID, oldName, newName, "replace")
+            del after[newName]
+            continue
+
+        fuzzyOldName = getFuzzyName(oldName)
+        if fuzzyOldName is not None:
+            for newName, newID in after.iteritems():
+                fuzzyNewName = getFuzzyName(newName)
+
+                found = False
+                if fuzzyOldName == fuzzyNewName:
+                    mapping[oldID] = (newID, oldName, newName, "fuzzy "+fuzzyNewName)
+                    del after[newName]
+                    found = True
+                    break
+                if found: break
+            if found: continue
+
+        mapping[oldID] = (None, oldName, None, "no match")
+
+    return mapping
+
+def getFuzzyName(name):
+    try:
+        import fuzzy # sudo easy_install fuzzy - fast Python phonetic algorithms: https://pypi.python.org/pypi/Fuzzy
+    except ImportError:
+        return None
+
+    return fuzzy.DMetaphone()(name)[0]
+
 def main():
     if len(sys.argv) < 3:
         print "usage: %s first-nei-dump second-nei-dump" % (sys.argv[0],)
         sys.exit(1)
 
-    print readNEIDump(sys.argv[1])
-    print readNEIDump(sys.argv[2])
+    before = readNEIDump(sys.argv[1])
+    after = readNEIDump(sys.argv[2])
+
+    mapping = matchAll(before, after)
+
+    for k, v in mapping.iteritems():
+        print k,v
+
+ 
 
 if __name__ == "__main__":
     main()
