afe4f0abf4596c73b8c5071c88e46e3ce147e305
Agaricus <agaricusb@yahoo.com>
2013-04-30 20:59:35 -0700

    Detect doubly-mapped transitive mappings (a->b->c) in patch file), which usually are in error

---
+++ checktrans.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+# Check for 'transitive' conversions in patch file:
+#   a -> b -> c
+# usually unintentional..
+
+def readPatchFile(fn):
+    maps = []
+    for line in file(fn, "rt").readlines():
+        line = line.replace("\n", "")
+        if "#" in line: line = line[:line.index("#")]
+        tokens = line.split(" -> ")
+        if len(tokens) != 2: continue
+        before, after = tokens
+        before = before.strip()
+        after = after.strip()
+       
+        maps.append((before, after))
+    return maps
+
+def main():
+    maps = readPatchFile("patchfile.txt")
+    converted = {}
+    for before, after in maps:
+        if converted.has_key(before): # TODO: check for metadata
+            print "Transitive conversion: %s -> %s -> %s" % (converted[before], before, after)
+
+        converted[after] = before
+
+
+if __name__ == "__main__":
+    main()
+        
