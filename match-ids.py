bf3b32caa17ad090f16a320f5e1696012815aa8d
Agaricus <agaricusb@yahoo.com>
2019-01-28 18:30:59 -0800

    Parse 1.7.10 NEI dumps csv directories

--- match-ids.py
+++ match-ids.py
@@ -1,11 +1,58 @@
 #!/usr/bin/python
 
+# python match-ids.py IDMap_dump-AA152-28-1-2019\ at\ 17.38.21.625.txt 1710-dumps /Applications/MultiMC.app/Contents/MacOS/instances/1.5.2AA\ \(Migrated\)1/minecraft/config/ /Applications/MultiMC.app/Contents/MacOS/instances/1.7.10s/minecraft/config
+
 import sys
 import os
 import re
 
+VANILLA_BLOCK_END = 158 # 1.5.2 dropper
+VANILLA_ITEM_END = 408 # 1.5.2
+
+global id2info
+
+"""Read 1.7.10 circa NEI dumps, directory of .csv files"""
+def readNEIDumpDir(dir):
+    global id2info
+    def readCSV(fn, name2id, id2info, skip_id_le):
+        f = file(fn, "rt")
+        for line in f.readlines():
+            if line.startswith("Name,ID,"): continue
+            name, id, has_itemblock, mod, cls = line.strip().split(",")
+            id = int(id)
+
+            if id <= skip_id_le:
+                # skip vanilla IDs since they don't change
+                continue
+
+            if name2id.has_key(name) and id != name2id[name]:
+                print "Duplicate name %s assigned to both id %s and %s" % (name, name2id[name], id)
+                raise SystemExit
+            name2id[name] = id
+
+            if id2info.has_key(id) and name != id2info[id][0]:
+                print "Duplicate id %s assigned to both %s and %s" % (id, id2info[id], (name, mod, cls))
+                raise SystemExit
+            if mod == "null": mod = ""
+            id2info[id] = (name, mod, cls)
+
+        return name2id, id2info
+
+    name2id = {}
+    id2info = {}
+
+    readCSV(os.path.join(dir, "block.csv"), name2id, id2info, VANILLA_BLOCK_END)
+    readCSV(os.path.join(dir, "item.csv"), name2id, id2info, VANILLA_ITEM_END) 
+
+    #import pprint
+    #pprint.pprint(id2info)
+    #raise SystemExit
+    return name2id
+
 """Load an NEI ID map dump into a dictionary of unlocalized name -> numeric ID"""
 def readNEIDump(fn):
+    if os.path.isdir(fn): return readNEIDumpDir(fn)
+
     m = {}
     # based on ModAnalyzer
     for line in file(fn).readlines():
@@ -14,6 +61,10 @@ def readNEIDump(fn):
         if line.startswith("Block. Name: ") or line.startswith("Item. Name: "):
             kind, info = line.split(": ", 1)
             unlocalizedName, id = info.split(". ID: ")
+            id = int(id)
+
+            if line.startswith("Block. Name: ") and id <= VANILLA_BLOCK_END: continue
+            if line.startswith("Item. Name: ") and id <= VANILLA_ITEM_END: continue
 
             while m.has_key(unlocalizedName):
                 unlocalizedName += "_" # overloaded name
@@ -24,6 +75,14 @@ def readNEIDump(fn):
 
 def matchAll(before, after, configsBefore, configsAfter):
     mapping = {}
+
+    after_lastToken = {}
+    for k in after.keys():
+        last = k.split(":")[-1]
+        while after_lastToken.has_key(last):
+            #print "Duplicate last token %s both %s and %s" % (last, after_lastToken[last], k)
+            last += "_"
+        after_lastToken[last] = (after[k], k)
 
     for oldName, oldID in before.iteritems():
         # check NEI dumps first
@@ -42,6 +101,22 @@ def matchAll(before, after, configsBefore, configsAfter):
             mapping[oldID] = (newID, oldName, newName, "replace")
             del after[newName]
             continue
+
+        newName = oldName.replace("item.", "").replace("item.", "")
+        if after.has_key(newName):
+            newID = after[newName]
+            mapping[oldID] = (newID, oldName, newName, "replace")
+            del after[newName]
+            continue
+
+        # TODO: try again matching on last token? kinda too ambiguous, now namespaced to mod, can overload
+        #lastToken = oldName.split(".")[-1]
+        #if after_lastToken.has_key(lastToken):
+        #    newID, newName = after_lastToken[lastToken]
+        #    mapping[oldID] = (newID, oldName, newName, "last")
+        #    print "Match", newName
+        #    del after[newName]
+        #    continue
 
         # search configs for likely configuration names, by ID
         possibleOldNames = []
