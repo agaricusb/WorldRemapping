de9d8d7c702c57756a7aad242e93ca682f77cbba
Agaricus <agaricusb@yahoo.com>
2019-02-07 18:59:22 -0800

    missing.py: lookup in NEI ID dumps, 1.5.2 and if still missing then 1.2.5

--- missing.py
+++ missing.py
@@ -3,6 +3,7 @@
 # Lookup missing mappings in patchfile for the user to fix
 
 patchfile = file("patchfile.txt").readlines()
+match_ids = __import__("match-ids")
 
 ids = {}
 for line in file("un").readlines():
@@ -21,9 +22,19 @@ def findPatchfile(id):
             return line.strip()
     return None
 
+dump152 = match_ids.readNEIDump("IDMap_dump-AA152-28-1-2019 at 17.38.21.625.txt")
+dump125 = match_ids.readNEIDump("IDMap_dump-125.txt")
+
+rdump152 = {v: k for k, v in dump152.items()}
+rdump125 = {v: k for k, v in dump125.items()}
+
 for id in sorted(ids):
     count = ids[id]
 
     found = findPatchfile(id)
+    if found is None:
+        maybe125 = rdump125.get(id)
+    else:
+        maybe125 = ""
 
-    print id, count, found
+    print id, count, found, rdump152.get(id), maybe125
