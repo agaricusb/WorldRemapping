3f93fd0def4ac196ad4bb6723b19c9153e5db37c
Agaricus <agaricusb@yahoo.com>
2019-04-14 18:14:35 -0700

    Add vanilla ID conversion, go script, no potatoes example, saved dumps

---
+++ go.sh
@@ -0,0 +1,2 @@
+#!/bin/sh -x
+python match-ids.py IDMap_dump-AA152-28-1-2019\ at\ 17.38.21.625.txt 1710-dumps /Applications/MultiMC.app/Contents/MacOS/instances/1.5.2AA\ \(Migrated\)1/minecraft/config/ /Applications/MultiMC.app/Contents/MacOS/instances/1.7.10s/minecraft/config
